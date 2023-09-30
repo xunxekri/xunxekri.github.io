1248667934643100c5da9a6549af5c113f2fb4fd
xunxekri <xunxekri@gmail.com>
2023-09-30 12:23:39 -0500

    Tweaked set downloader

--- set-downloader/main.js
+++ set-downloader/main.js
@@ -17,48 +17,50 @@ const $download = document.querySelector('#download');
 const $images = document.querySelector('#images');
 const $imageType = document.querySelector('#image-type');
 const $progress = document.querySelector('#progress');
+const $numberCheckbox = document.querySelector('#number-in-name');
 
 const sets = setlistData.data.filter(sset => !exlcudeCriteria.test(sset.set_type));
 
 let setNameToSet = new Map();
-//let setCodeToName = new Map();
 sets.forEach(sset => {
 	if(setNameToSet.has(sset.name)) throw `Set name ${sset.name} declared twice`;
-	setNameToSet.set(sset.name, sset);
-	setNameToSet.set(sset.code, sset);
+	setNameToSet.set(sset.name.toLowerCase(), sset);
+	setNameToSet.set(sset.code.toLowerCase(), sset);
 
 	const $el = document.createElement('option');
 	$el.value = sset.name;
 	$datalist.appendChild($el);
 });
 
-const getImagesAndNames = type => card => {
+const getCardInfo = imageType => card => {
 	if(card.card_faces && card.card_faces[0].image_uris) {
-		return card.card_faces.map((face, i) => {return {image: face.image_uris[type], name: face.name}});
+		return card.card_faces.map((face, i) => {return {image: face.image_uris[imageType], name: face.name, number: card.collector_number}});
 	}
 
-	return [{image: card.image_uris[type], name: card.name}];
+	return [{image: card.image_uris[imageType], name: card.name, number: card.collector_number}];
 }
 
 $input.addEventListener('input', async() => {
 	$progress.style.opacity = 0;
 	$images.innerHTML = '';
 	const setName = $input.value;
-	if(!setNameToSet.has(setName)) return;
+	if(!setNameToSet.has(setName.toLowerCase())) return;
 
 	let has_more = true;
 	let setReqs = [];
-	let next_page = `https://api.scryfall.com/cards/search?q=in:paper%20s:${setNameToSet.get(setName).code}`;
+	let next_page = `https://api.scryfall.com/cards/search?q=in:paper%20s:${setNameToSet.get(setName.toLowerCase()).code}`;
 	while(has_more) {
 		let newData = await safeFetch(next_page).then(data => data.json());
 		has_more = newData.has_more;
 		next_page = newData.next_page;
 		setReqs.push(newData);
 	}
-	setReqs.flatMap(req => req.data).flatMap(getImagesAndNames($imageType.value)).forEach(card => {
+	setReqs.flatMap(req => req.data).flatMap(getCardInfo($imageType.value)).forEach(card => {
 		let $img = document.createElement('img');
+		let prefix = $numberCheckbox.checked ? `${card.number} `.padStart(4, '0') : '';
 		$img.src = card.image;
-		$img.alt = card.name;
+		$img.alt = prefix + card.name;
+		if($numberCheckbox.checked) $img.style.order = card.number;
 		$images.appendChild($img);
 	});
 });
@@ -66,6 +68,7 @@ $input.addEventListener('input', async() => {
 $input.dispatchEvent(new Event('input'));
 
 $imageType.addEventListener('input', () => $input.dispatchEvent(new Event('input')));
+$numberCheckbox.addEventListener('input', () => $input.dispatchEvent(new Event('input')));
 
 $download.addEventListener('click', async() => {
 	if(!$images.firstChild) return;
